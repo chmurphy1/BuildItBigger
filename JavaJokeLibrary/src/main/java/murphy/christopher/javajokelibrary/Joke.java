db01137dd808c21d597c87b3e92352768b02344a
Christopher Murphy <christopher.w.murphy1@gmail.com>
2019-01-17 18:17:59 -0700

    Adding jokes to the joke file

--- JavaJokeLibrary/src/main/java/murphy/christopher/javajokelibrary/Joke.java
+++ JavaJokeLibrary/src/main/java/murphy/christopher/javajokelibrary/Joke.java
@@ -1,4 +1,23 @@
 package murphy.christopher.javajokelibrary;
 
+import java.util.ArrayList;
+
 public class Joke {
+
+    private static ArrayList<String> jokes = new ArrayList<>();
+    static{
+        jokes.add("Why did the tree go to the dentist?\n To get a root canal");
+        jokes.add("How did the telephone propose to its girlfriend?\n He gave her a ring.");
+        jokes.add(" I quit my job at the helium factory today.\n I refuse to be spoken to in that tone of voice.");
+        jokes.add("Corny jokes are a-maize-ing.");
+        jokes.add("What type of market should you NEVER take your dog?\n A flea market!");
+        jokes.add("Today at the bank, an old lady asked me to help check her balance. So I pushed her over.");
+        jokes.add("My wife told me I had to stop acting like a flamingo. So I had to put my foot down.");
+    }
+    public static String getJoke(){
+        int index = (int)(Math.random() * jokes.size()) + 1;
+
+        return jokes.get(index);
+    }
+
 }
