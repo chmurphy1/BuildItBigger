4a8fa3c03625f7ee7307975f14932771be693b10
Christopher Murphy <christopher.w.murphy1@gmail.com>
2019-01-20 13:18:05 -0700

    Changed the server endpoint

--- backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
+++ backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
@@ -3,7 +3,7 @@ package com.udacity.gradle.builditbigger.backend;
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiNamespace;
-
+import murphy.christopher.javajokelibrary.Joke;
 import javax.inject.Named;
 
 /** An endpoint class we are exposing */
@@ -18,12 +18,10 @@ import javax.inject.Named;
 )
 public class MyEndpoint {
 
-    /** A simple endpoint method that takes a name and says Hi back */
-    @ApiMethod(name = "sayHi")
-    public MyBean sayHi(@Named("name") String name) {
+    @ApiMethod(name = "getAJoke")
+    public MyBean getAJoke() {
         MyBean response = new MyBean();
-        response.setData("Hi, " + name);
-
+        response.setData(Joke.getJoke());
         return response;
     }
 
