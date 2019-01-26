67679877d9c9d99d28ae8e30724970d09819e3a0
Christopher Murphy <christopher.w.murphy1@gmail.com>
2019-01-26 14:44:38 -0700

    Fixed JokeTaskTest so it fails if the server is not running

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/JokeTaskTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/JokeTaskTest.java
@@ -5,18 +5,24 @@ import com.udacity.gradle.builditbigger.tasks.JokeTask;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import android.support.test.runner.AndroidJUnit4;
+
+import java.util.concurrent.ExecutionException;
+
 import static org.junit.Assert.assertFalse;
 
 @RunWith(AndroidJUnit4.class)
 public class JokeTaskTest {
 
     @Test
-    public void TestJokeTask(){
-        new JokeTask(new JokeInterface() {
+    public void TestJokeTask() throws ExecutionException, InterruptedException {
+
+        String joke = new JokeTask(new JokeInterface() {
             @Override
             public void displayJoke(String joke) {
-               assertFalse ("".equals(joke));
+
             }
-        }).execute();
+        }).execute().get();
+
+        assertFalse ("".equals(joke));
     }
 }
